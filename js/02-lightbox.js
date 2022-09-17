8331f9a703c5f1601cb8b29fd4b31d7ef9935b1e
SergeyPochapskiy <104765308+SergeyPochapskiy@users.noreply.github.com>
2022-09-17 23:35:59 +0300

    ready lighhtbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery')
+const itemsMarkup = galleryItems.map(element => {
+	const galleryLink = document.createElement('a')
+	galleryLink.className = 'gallery__link'
+	galleryLink.href = element.original
+	const galleryImage = document.createElement('img')
+	galleryImage.className = 'gallery__image'
+	galleryImage.src = element.preview
+	galleryImage.setAttribute('title', element.description)
+	galleryImage.alt = element.description
+
+	galleryLink.append(galleryImage)
+	return galleryLink
+})
+galleryRef.append(...itemsMarkup)
+
+new SimpleLightbox('.gallery a', {
+	captionDelay: 250
+})
